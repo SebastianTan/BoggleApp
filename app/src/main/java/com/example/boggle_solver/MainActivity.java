a52a30fd39e0ac05c018e6762d6cdc6d4debd458
Sebastian Tan <sprotan@gmail.com>
2021-09-03 14:34:56 -0700

    Production ready with dictionary

--- app/src/main/java/com/example/boggle_solver/MainActivity.java
+++ app/src/main/java/com/example/boggle_solver/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.boggle_solver;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -16,31 +23,45 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    final boolean DEBUG = true;
+    final boolean DEBUG = false;
     ArrayList<Integer> sectionHeaders = new ArrayList<Integer>();
     int scroll = 0;
     final int NEWLINES = 1;
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.textView2);
+        TextView textView = findViewById(R.id.wordList);
         textView.setMovementMethod(new ScrollingMovementMethod());
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        myToolbar.inflateMenu(R.menu.menu);
+        setSupportActionBar(myToolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
     // Runs on button press
     public void sendMessage(View view) {
 
 
-        TextView textView = findViewById(R.id.textView2);
+        TextView textView = findViewById(R.id.wordList);
         textView.scrollTo(0,0);
 
         String output = ""; //button output text;
 
         // Get input text
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = (EditText) findViewById(R.id.boggleInput);
         String message = editText.getText().toString();
 //        output += (message + "\n\n"); //DEBUG
 
@@ -48,16 +69,31 @@ public class MainActivity extends AppCompatActivity {
         double m_length = Math.sqrt(message.length());
         if( m_length != Math.floor(m_length)) {
             output = "Please check that your board input is the right size!";
-            textView = findViewById(R.id.textView2);
+            textView = findViewById(R.id.wordList);
             textView.setText(output);
             return;
         }
 
+        SharedPreferences sP = this.getSharedPreferences(getString(R.string.preference_file_key),Context.MODE_PRIVATE);
+        int dictFilename = sP.getInt(getString(R.string.preference_file_key),R.raw.scrabble);
+
+        switch(dictFilename) {
+            case R.raw.scrabble:
+                output += "Using Scrabble dictionary \n";
+                break;
+            case R.raw.yawl:
+                output += "Using Awesome Word List \n";
+                break;
+            default:
+                output += "Error\n";
+
+
+        }
 
         String dictionary[] = {};
 
         StringBuffer buf = new StringBuffer();
-        InputStream is = this.getResources().openRawResource(R.raw.scrabble);
+        InputStream is = this.getResources().openRawResource(dictFilename);
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         List<String> dict = new ArrayList<String>();
         String data = null;
@@ -104,8 +140,30 @@ public class MainActivity extends AppCompatActivity {
             }
             output += (s + "\n");
         }
-        textView = findViewById(R.id.textView2);
+        textView = findViewById(R.id.wordList);
         textView.setText(output);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                openOptions();
+                return true;
+
+        }
+        return false;
+    }
+
+
+
+    // Navigation
+    void openOptions() {
+        Intent intent = new Intent(this, OptionsMenu.class);
+        startActivity(intent);
 
     }
     public void upSection(View view){
@@ -126,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
         } else if (scroll > sectionHeaders.size() - 1){
             scroll = sectionHeaders.size() - 1;
         } else {
-            TextView textView = findViewById(R.id.textView2);
+            TextView textView = findViewById(R.id.wordList);
             int pos = textView.getLayout().getLineTop(sectionHeaders.get(scroll));
             textView.scrollTo(0,pos);
         }
--- app/src/main/java/com/example/boggle_solver/OptionsMenu.java
+++ app/src/main/java/com/example/boggle_solver/OptionsMenu.java
@@ -1,22 +1,69 @@
 package com.example.boggle_solver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+public class OptionsMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        setContentView(R.layout.options_menu);
 
         Intent intent = getIntent();
+
+        Toolbar optionsToolbar = (Toolbar) findViewById(R.id.optionsToolbar);
+        optionsToolbar.inflateMenu(R.menu.back);
+        setSupportActionBar(optionsToolbar);
         //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        String message = "hi";
-        TextView textView = findViewById(R.id.Hello);
-        textView.setText(message);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.back, menu);
+        return true;
+    }
+    public void dict1(View view){
+        updateDict(1);
+    }
+    public void dict2(View view){
+        updateDict(0);
+    }
+
+    public void updateDict(int i) {
+        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        switch(i) {
+            case 1:
+                editor.putInt(getString(R.string.preference_file_key), R.raw.yawl);
+                break;
+            default:
+                editor.putInt(getString(R.string.preference_file_key), R.raw.scrabble);
+        }
+        editor.apply();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.settings: {
+                finish();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
